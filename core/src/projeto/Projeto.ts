787f01fd38e7f05b9e91c1a787416354e48593e2
Felix <felipedasilvapereira@outlook.com>
2025-03-19 11:16:52 -0300

    feat(core): modelo implementado

---
+++ core/src/projeto/Projeto.ts
@@ -0,0 +1,16 @@
+import { Tipo } from './Tipo'
+import { Nivel } from './Nivel'
+import Tecnologia from '../tecnologia/Tecnologia'
+
+export default interface Projeto{
+    id: number
+    nome: string
+    descricao: string
+    imagens: string[]
+    tipo: Tipo
+    nivel: Nivel
+    repositorio: string
+    destacado: boolean
+    tecnologias: Tecnologia[]
+}
+    
---
+++ core/src/projeto/index.ts
@@ -0,0 +1,6 @@
+import { Nivel } from "./Nivel";
+import { Tipo } from "./Tipo";
+import Projeto from "./Projeto";
+
+export {Tipo, Nivel}
+export type { Projeto }
