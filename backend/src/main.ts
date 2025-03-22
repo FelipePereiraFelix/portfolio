69fa36138432787f7256a9f015385d14f5dcef09
Felix <felipedasilvapereira@outlook.com>
2025-03-22 00:42:31 -0300

    ajuste(backend/main): alteração dos dados para que deploy funcione

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 3000;
+  console.log(`Application is running on port: ${port}`); // Log para verificar a porta
+
+  await app.listen(port);
 }
+
 bootstrap();
