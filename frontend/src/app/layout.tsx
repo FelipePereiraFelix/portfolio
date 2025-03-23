f97a330a63138130d944d43370bacdbd7c7f5c65
Felix <felipedasilvapereira@outlook.com>
2025-03-22 21:24:55 -0300

    feat(frontend): versão inicial do projeto

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Portfolio de projeto de desenvolvimento web",
+};
+
+const fonte = Montserrat({
+ subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${fonte.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
