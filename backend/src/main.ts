c4dc505557c54596f91bc80e09ab0b5485cdb1a4
Felix <felipedasilvapereira@outlook.com>
2025-03-22 21:26:59 -0300

    change(backend): Alteração da porta para uso do frontend na 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from "@nestjs/core";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   console.log(`Application is running on port: ${port}`); // Log para verificar a porta
 
   await app.listen(port);
